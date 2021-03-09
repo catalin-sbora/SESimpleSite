5c8fd443114ae18b6b3ebaa638c741425a59d5e0
catalin-sbora <catalin.sbora@gmail.com>
2021-03-09 05:35:49 -0800

    created project

---
+++ SimpleSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpleSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
